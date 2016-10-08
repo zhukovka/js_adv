2a9fc5cebcb311161cb4a4f49ee6a116da6c474d
zhukovka <helen.zhukova@gmail.com>
2016-10-08 15:05:10 +0300

    modules promise

---
+++ promises/task1.js
@@ -0,0 +1,12 @@
+/*
+ ## Task
+
+ For this first lesson, let’s review what we already know about asynchronous
+ operations in JavaScript.
+
+ Using setTimeout, print the string 'TIMED OUT!' after 300ms.
+
+ */
+setTimeout(function () {
+    console.log('TIMED OUT!');
+}, 300);
